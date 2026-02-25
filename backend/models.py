154b3b5ec8c1b962d93ddeffc00266983687a8e0
iajwork <indrajit.joshi@oneture.com>
2026-02-25 16:48:32 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,26 +1,33 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, Date, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, Float, DateTime, Date, ForeignKey, Text, Boolean
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
+
+class LocationMaster(Base):
+    __tablename__ = "location_master"
+    location_id = Column(Integer, primary_key=True, index=True)
+    location_name = Column(String(150), nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    radius = Column(Integer, nullable=False, default=100)
 
 class EmployeeMaster(Base):
     __tablename__ = "employee_master"
     employee_id = Column(Integer, primary_key=True, index=True)
     employee_code = Column(String(20), nullable=False, unique=True)
     employee_name = Column(String(150), nullable=False)
-    client_mapping = Column(String(150))
-    work_mode = Column(String(20))
-    status = Column(String(20), default="Active")
+    assigned_location_id = Column(Integer, ForeignKey("location_master.location_id"))
+    is_valid = Column(Boolean, default=True)
 
 class ClockLogs(Base):
     __tablename__ = "clock_logs"
     log_id = Column(Integer, primary_key=True, index=True)
     employee_id = Column(Integer, ForeignKey("employee_master.employee_id"), nullable=False)
     punch_timestamp = Column(DateTime, nullable=False)
-    location_name = Column(String(150), nullable=False, default="Unknown")
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     punch_status = Column(String(10))
+    is_valid = Column(Boolean) # Geofencing result
     device_identifier = Column(String(100))
     address = Column(Text)
 
@@ -32,15 +39,5 @@ class DailyAttendance(Base):
     login_time = Column(DateTime)
     logout_time = Column(DateTime)
     total_working_hours = Column(Float)
-    status = Column(String(20)) 
+    is_valid = Column(Boolean)
     created_at = Column(DateTime, default=datetime.utcnow)
-
-class MonthlySummary(Base):
-    __tablename__ = "monthly_summary"
-    summary_id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(Integer, ForeignKey("employee_master.employee_id"), nullable=False)
-    month = Column(Integer, nullable=False)
-    year = Column(Integer, nullable=False)
-    office_days = Column(Integer, default=0)
-    remote_days = Column(Integer, default=0)
-    total_days_present = Column(Integer, default=0)
